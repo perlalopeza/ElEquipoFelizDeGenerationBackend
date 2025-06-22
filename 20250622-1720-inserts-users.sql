b1fed9bffcd4a030f28ffbf99813e1b8cbc49a77
Mauricio Cid <cid.cruz.mauricio@gmail.com>
2025-06-22 17:22:58 -0600

    feat: inserciones en la tabla de users

---
+++ 20250622-1720-inserts-users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (name, last_name, phone, email, password, privileges_id) VALUES 
+('Maria', 'Alvarez', '5372918583', 'alvamar01@gmail.com', 'Elpatitofeo01#', 2), 
+('Ricardo', 'Caballero', '5395847195', 'cabaric02@gmail.com', 'Elpatitobonito02#', 3),
+('Mauricio', 'Cid', '5371264532', 'cidmau03@gmail.com', 'Lasmañanitas03#', 3),
+('Rene', 'Cruz', '5387090554', 'cruzrene04@gmail.com', 'Laptopderene04#', 1);
+
+SELECT * FROM users;
