47bff8c439dc44994502080da3c66f49ddc8c9ce
Rene Cruz <m144renecruz2041@gmail.com>
2025-06-23 18:28:43 -0600

    feat: inserción de datos en la tabla user_addresses

---
+++ 23062025-1826-inserts-addresses.sql
@@ -0,0 +1,9 @@
+SELECT * FROM user_addresses;
+
+INSERT INTO user_addresses (street_and_number, neighborhood, zip_code, town, state, users_id)
+VALUES
+('Av. Insurgentes Sur 1234', 'Del Valle', '03100', 'Ciudad de México', 'CDMX', 1),
+('Calle 5 de Mayo #45', 'Centro', '20000', 'Aguascalientes', 'Aguascalientes', 2),
+('Blvd. Miguel Hidalgo 789', 'Zona Río', '22010', 'Tijuana', 'Baja California', 3),
+('Av. Juárez 321', 'Centro Histórico', '68000', 'Estado de México', 'Estado de México', 4);
+
