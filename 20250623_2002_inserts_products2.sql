7e5c2f7481e31dd5c39350d94daba400ed80e055
Maria Alvarez <marigpe.9716@gmail.com>
2025-06-23 20:05:28 -0600

    fix: correccion de insert-product2

---
+++ 20250623_2002_inserts_products2.sql
@@ -0,0 +1,8 @@
+select * from products;
+INSERT INTO products (product_name, price, description, category_id, stock, discount) 
+VALUES 
+("KIT DE RIEGO AUTOMÁTICO", 1850, "Sistema completo para irrigación controlada en invernaderos pequeños o medianos.", 1, 75, 0.15),
+("PLÁSTICO AGRÍCOLA TRICAPA", 3200, "Película plástica de alta resistencia, ideal para proteger cultivos de clima extremo.", 2, 50, 0.10),
+("TERMÓMETRO DIGITAL CON SONDA", 490, "Permite monitorear con precisión la temperatura ambiente o del sustrato.", 1, 200, 0.05),
+("BANDEJA DE GERMINACIÓN 200 CAVIDADES", 120, "Diseñada para una germinación eficiente con sistema de drenaje incorporado.", 2, 500, 0.20),
+("MALLA ANTIÁFIDOS 40 MESH", 2850, "Protección eficaz contra insectos pequeños sin restringir la ventilación.", 1, 60, 0.10);
