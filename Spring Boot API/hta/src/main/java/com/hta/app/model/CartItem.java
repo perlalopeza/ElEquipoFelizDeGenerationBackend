5664c839b006ef4a723fedfd3ca46c677d135485
Rene Cruz <m144renecruz2041@gmail.com>
2025-06-27 18:28:11 -0600

    fix: correcion de archivo cart_item

--- Spring Boot API/hta/src/main/java/com/hta/app/model/CartItem.java
+++ Spring Boot API/hta/src/main/java/com/hta/app/model/CartItem.java
@@ -1,5 +1,7 @@
 package com.hta.app.model;
 
+import java.math.BigDecimal;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,16 +15,18 @@ import jakarta.persistence.Table;
 public class CartItem {
 
 
-    private Long id;
 
-    private Integer quantity;
+	@ManyToOne
+	@JoinColumn(name = "products_id", nullable = false)
+	private Product product;
 
     @ManyToOne
     @JoinColumn(name = "shopping_cart_id", nullable = false)
     private ShoppingCart shoppingCart;
 
-    @ManyToOne
-    @JoinColumn(name = "products_id", nullable = false)
-    private Product product;
+    
+    private Integer quantity;
+    
+    private BigDecimal priceAtPurchase;    
     
 }
