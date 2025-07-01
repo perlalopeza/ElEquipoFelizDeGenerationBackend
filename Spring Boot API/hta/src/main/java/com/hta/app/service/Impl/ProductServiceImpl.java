59e07e746752bce64b4bcc70562e18207ffc5c74
Maria Alvarez <marigpe.9716@gmail.com>
2025-06-30 18:14:45 -0600

    feat: se genero el archivo ProductRepository

---
+++ Spring Boot API/hta/src/main/java/com/hta/app/service/Impl/ProductServiceImpl.java
@@ -0,0 +1,48 @@
+package com.hta.app.service.Impl;
+
+import java.util.Set;
+
+import com.hta.app.model.Product;
+import com.hta.app.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+
+	//ProductRepository  productRepository;
+
+	@Override
+	public Iterable<Product> findAll() {
+	  
+		return null;
+	}
+
+	@Override
+	public Product findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product save(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product update(Long id, Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<String> getUsersWithSpecificProductId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
