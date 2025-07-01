951d1ee789addf86fa017acb3e6dec6df5b9d2c4
RicardoCaballero26 <richycaballero14@gmail.com>
2025-07-01 16:22:27 -0600

    feat: creacion de que UserService

---
+++ Spring Boot API/hta/src/main/java/com/hta/app/service/UserService.java
@@ -0,0 +1,14 @@
+package com.hta.app.service;
+
+import java.util.Set;
+
+import com.hta.app.model.User;
+
+public interface UserService {
+	Iterable<User> findAll();
+	User findById(Long id);
+	User save(User user);
+	User update(Long id, User user);
+	void deleteById(Long id);
+	Set<String> getUsersWithSpecificUserId(Long id);
+}
