8c3433e9927720396c111b3c0fea74f44dc282b8
Maria Alvarez <marigpe.9716@gmail.com>
2025-06-30 18:50:09 -0600

    feat: se creo el productServiceImpl

--- Spring Boot API/hta/src/main/java/com/hta/app/service/Impl/ProductServiceImpl.java
+++ Spring Boot API/hta/src/main/java/com/hta/app/service/Impl/ProductServiceImpl.java
@@ -1,47 +1,71 @@
 package com.hta.app.service.Impl;
 
+import java.util.Optional;
 import java.util.Set;
 
 import com.hta.app.model.Product;
+import com.hta.app.repository.ProductRepository;
 import com.hta.app.service.ProductService;
 
 public class ProductServiceImpl implements ProductService {
 
-	//ProductRepository  productRepository;
+	ProductRepository  productRepository;
+     
+	public ProductServiceImpl(ProductRepository productRepository) {
+		super();
+		this.productRepository = productRepository;
+	}
 
 	@Override
 	public Iterable<Product> findAll() {
 	  
-		return null;
+		return productRepository.findAll();
 	}
 
 	@Override
 	public Product findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		
+			Optional<Product> productOpt = productRepository.findById(id);
+			if(productOpt.isEmpty()) { 
+				throw new IllegalStateException("Product with id: "+ id +" does not exist");
+			}
+			Product existingProduct = productOpt.get();
+			return existingProduct;
+		};
+	
 
 	@Override
 	public Product save(Product product) {
-		// TODO Auto-generated method stub
-		return null;
+		product.setId(null); 
+		Product newProduct = productRepository.save(product);
+		
+		return newProduct;
 	}
 
 	@Override
 	public Product update(Long id, Product product) {
-		// TODO Auto-generated method stub
-		return null;
+		Product existingProduct = findById(id);
+		
+		existingProduct.setProductName(product.getProductName());
+		existingProduct.setDescription(product.getDescription());
+		existingProduct.setCategory(product.getCategory());
+		existingProduct.setDiscount(product.getDiscount());
+		existingProduct.setImage(product.getImage());
+		existingProduct.setPrice(product.getPrice());
+		existingProduct.setStock(product.getStock());
+		Product updatedProduct = productRepository.save(existingProduct);
+		return updatedProduct;
 	}
 
 	@Override
 	public void deleteById(Long id) {
-		// TODO Auto-generated method stub
+		Product existingProduct = findById(id);
+		productRepository.delete(existingProduct);
 		
 	}
 
 	@Override
 	public Set<String> getUsersWithSpecificProductId(Long id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
