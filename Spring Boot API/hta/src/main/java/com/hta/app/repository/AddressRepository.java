60e65716e6a1ca8a1b0f6b1169a2dbaa9b7f430c
Fernando Lopez <spartan.fernando16@gmail.com>
2025-07-01 18:10:10 -0600

    feat: Se crea archivo addressRepository

---
+++ Spring Boot API/hta/src/main/java/com/hta/app/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.hta.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hta.app.model.Address;
+
+public interface AddressRepository extends CrudRepository <Address, Long>{
+
+}
+ 
