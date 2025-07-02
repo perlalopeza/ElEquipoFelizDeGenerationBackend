2259c54270e416bf6abb60b74084e0cd7fcb0370
Luis Eduardo Varela <EduardoVarela_1996@hotmail.com>
2025-07-01 20:41:42 -0600

    fix: Se agrega la notación de Mapeo a CartItem

--- Spring Boot API/hta/src/main/java/com/hta/app/controller/CartItemController.java
+++ Spring Boot API/hta/src/main/java/com/hta/app/controller/CartItemController.java
@@ -3,16 +3,23 @@ package com.hta.app.controller;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.hta.app.model.CartItem;
 import com.hta.app.model.CartItemId;
 import com.hta.app.model.Product;
 import com.hta.app.service.CartItemService;
 
+@RestController
+@RequestMapping("/api/v1/cartItems")
+@CrossOrigin(origins = "*")
 public class CartItemController {
 	
 	CartItemService cartItemService ;
